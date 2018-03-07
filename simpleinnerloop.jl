c05b442ff1d28c777c406091817e889dfaba5c1c
Yan-Ping <yamp000@hotmail.com>
2018-03-07 08:23:01 -0800

    PM2.5 Behavior Fixed
    Added PM2.5 Exhaust Term - PM2.5 is also exhausted based on the running of the AHU fan.  Physically makes more sense.  Debug plots look correct.

--- simpleinnerloop.jl
+++ simpleinnerloop.jl
@@ -54,62 +54,62 @@ R = 0.8 #Efficiency of filter
 
 PM25_MAX = 35
 CO2_MAX = 800
-PM25_0 = 100
+PM25_0 = 0
 CO2_0 = AMB_CO2[1]
+
 ####################################
 ######### Initialize Model #########
 ####################################
 
-m = Model(solver=AmplNLSolver(joinpath(PATH_TO_SOLVERS,"knitro"), ["outlev=2", "ms_enable=1"]))
+m = Model(solver=AmplNLSolver(joinpath(PATH_TO_SOLVERS,"knitro"), ["outlev=1", "ms_enable=1"]))
 
 ####################################
 ######## Decision variables ########
 ####################################
 
-
 @variable(m, CO2[1:T+1] >= 0, start=400)
 @variable(m, PM25[1:T+1] >= 0, start=100)
 @variable(m, CMH[1:T] >= 0, start=1)
-@variable(m, N >= 0, start=1)
-@variable(m, A[1:T] >= 0)
+# @variable(m, N >= 0, start=1)
+@variable(m, roomPM25Sink[1:T] >= 0)
+@NLexpression(m, N, R/k*sum(roomPM25Sink[t] for t in 1:T))
 
 ######################################
 ######## Objective Functions #########
 ######################################
 
-#Minimize the total cost, equal to the sum of the cost of fan electricity over the operational period, plus the cost of PM2.5 filter replacements
-@objective(m, Min, Celec*P*sum(CMH[t] for t in 1:T) + Cfilter*N)
+# Minimize the total cost, equal to the sum of the cost of fan electricity over the operational period, plus the cost of PM2.5 filter replacements
+@NLobjective(m, Min, Celec*P*sum(CMH[t] for t in 1:T) + Cfilter*N)
 
 ######################################
 ############# Constraints ############
 ######################################
 
-#CO2 concentration in the room at time t is equal to the CO2 concentration in the room at t-1 plus (the CO2 mass introduced at t-1 minus the CO2 mass removed at t-1) divided by the room volume
-#Consider concentrations to be measured at the END of each hour
-@NLconstraint(m, [t=2:T+1], CO2[t] == CO2[t-1] + (roomCO2Source[101][t] - CMH[t]*(CO2[t]-AMB_CO2[t]))/roomData[101][2])
+# CO2 concentration in the room at time t is equal to the CO2 concentration in the room at t-1 plus (the CO2 mass introduced at t minus the CO2 mass removed at t) divided by the room volume
+# The constraint is structured this way because the HVAC system can only react to rising CO2 levels at time t, it cannot pre-emptively condition the air at t-1
+@NLconstraint(m, [t=2:T], CO2[t] == CO2[t-1] + (roomCO2Source[101][t] - CMH[t]*(CO2[t] - AMB_CO2[t]))/roomData[101][2])
 
-#PM2.5 concentration in the room at time t is equal to the PM 2.5 in the room at t-1 plus the mass of PM2.5 introduced at t-1 divided by the room volume
-@constraint(m, [t=2:T+1], PM25[t] == PM25[t-1] + (CMH[t]*AMB_PM2_5[t] - A[t])/roomData[101][2])
+# PM2.5 concentration in the room at time t is equal to the PM 2.5 in the room at t-1 plus the mass of PM2.5 introduced at time t minus the mass filtered at time t divided by the room volume
+@constraint(m, [t=2:T], PM25[t] == PM25[t-1] + (CMH[t]*AMB_PM2_5[t] - CMH[t]*PM25[t] - roomPM25Sink[t])/roomData[101][2])
 
-#set dummy initial condition to ambient concentrations
+# set dummy initial condition to ambient concentrations
 @constraint(m, PM25[1] == PM25_0)
 @constraint(m, CO2[1] == CO2_0)
 
-#CO2 and PM2.5 concentrations reset at the end of the cycle.  Is this really needed? - YP
+# CO2 and PM2.5 concentrations reset at the end of the cycle.  Is this really needed? - YP
 @constraint(m, PM25[T+1] == PM25[1])
 @constraint(m, CO2[T+1] == CO2[1])
 
-#CO2 and PM2.5 levels must not exceed maximum allowable levels.  Constraints do not apply to first timestep, since conditioning has not yet been applied.
-@constraint(m, [t=2:T+1], PM25[t] <= PM25_MAX)
-@constraint(m, [t=2:T+1], CO2[t] <= CO2_MAX)
+# CO2 and PM2.5 levels must not exceed maximum allowable levels.  Constraints do not apply to first timestep, since conditioning has not yet been applied.
+@constraint(m, [t=2:T], PM25[t] <= PM25_MAX)
+@constraint(m, [t=2:T], CO2[t] <= CO2_MAX)
 
-@constraint(m, N == R/k*sum(A[t] for t in 1:T))
 
 ######################################
 ########### Print and solve ##########
 ######################################
 
-print(m)
+#print(m)
 solve(m)
 
 CO2result = getvalue(CO2)
